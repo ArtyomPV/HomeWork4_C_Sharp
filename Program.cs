f781a9c6f93c1f9cb93d8069dea1bad687b7cee8
Artyom <itcoding76@gmail.com>
2022-12-12 23:17:24 +0200

    add 2d task

--- Program.cs
+++ Program.cs
@@ -10,26 +10,62 @@
 
 */
 
-void Exponentiation(int a, int b){
+// void Exponentiation(int a, int b){
 
-    int i = 0;
-    int exp = 1;
-    if(b<0) Console.WriteLine("a natural degree must not be less then ZERO.");
-    else
-    {
-        while(i<b)
+//     int i = 0;
+//     int exp = 1;
+//     if(b<0) Console.WriteLine("a natural degree must not be less then ZERO.");
+//     else
+//     {
+//         while(i<b)
+//         {
+//             exp = exp * a;
+//             i++;    
+//         }
+//         Console.Write(exp);
+//     }
+// }
+
+//  Console.WriteLine("Enter two numbers, first will be a number, second will be а natural degree");
+//  Console.Write("A number: ");
+//  int number = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("a natural degree: ");
+//  int naturalDegree = Convert.ToInt32(Console.ReadLine());
+
+// Exponentiation(number, naturalDegree);
+
+//==================== Task 2 =========================
+/*
+Задача 27: Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.(Массивы и строки использовать нельзя!!!)
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+/*
+Получаем многозначное число
+создаем метод, метод содердит переменную которая аккумулирует значение
+создаем цикл, в котором определяем остаток от 10, данный остаток складываем в переменную аккумулятор
+далее уменьшаем предоставленное число на 10 и проводим определение остатка заново, 
+пока переменная после уменьшения на 10 не станет равным нулю
+
+*/
+int Sum(int num)
+{
+        int sum = 0;
+        while(num>0)
         {
-            exp = exp * a;
-            i++;    
+            sum += num%10;
+            num = num/10;
         }
-        Console.Write(exp);
-    }
+        return sum;
 }
 
- Console.WriteLine("Enter two numbers, first will be a number, second will be а natural degree");
- Console.Write("A number: ");
- int number = Convert.ToInt32(Console.ReadLine());
- Console.Write("a natural degree: ");
- int naturalDegree = Convert.ToInt32(Console.ReadLine());
- 
-Exponentiation(number, naturalDegree);
+Console.Write("Enter any number from 0 to 2 147 483 647: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int summa = Sum(number);
+Console.WriteLine(summa);
+
